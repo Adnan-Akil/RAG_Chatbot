3b4cc221cdebcfe4aa5bfce36286c350dc197b67
Adnan-Akil <hypedbeats04@gmail.com>
2025-07-17 01:15:00 +0400

    slight change to the prompt

--- src/llm.py
+++ src/llm.py
@@ -1,7 +1,7 @@
 from langchain_ollama.llms import OllamaLLM
 
 def final_answer(retrieved_docs, user_query):
-  llm_model= OllamaLLM(model='llama3.2',temperature=0.0)
+  llm_model= OllamaLLM(model='llama3.2',temperature=0.1)
 
   template = f"""
   You are a helpful, precise assistant.
@@ -17,7 +17,7 @@ def final_answer(retrieved_docs, user_query):
   - Only answer using the context above.
   - If the context does not contain enough information, reply: "I’m sorry, but the context does not provide enough information to answer this question."
   - Write clearly and concisely.
-  - Use bullet points or headings if it improves clarity.
+  - Use bullet points or headings IF it improves clarity.
   - Do not add anything extra.
 
   Answer:
