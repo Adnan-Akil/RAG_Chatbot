a0461558b4a5002c1e9e1bcea34ab81a3c2bfef7
Adnan-Akil <hypedbeats04@gmail.com>
2025-07-16 14:05:04 +0400

    -

--- src/file_parser.py
+++ src/file_parser.py
@@ -2,7 +2,7 @@ from data_loader import combined_doc
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain_huggingface import HuggingFaceEmbeddings
 from sentence_transformers import SentenceTransformer
-from langchain_community.vectorstores import Chroma
+from langchain_chroma import Chroma
 
 def chunker(combined_document):
   splitter= RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=200)
@@ -14,7 +14,7 @@ def chunker(combined_document):
   for i, chunk in enumerate(chunked_docs[:]):
     print(f"\n--- Chunk {i + 1} ---")
     print(chunk.page_content[:]) 
-  """ 
+  """  
   return chunked_docs
 
 def vector_embedding(chunked_docs):
@@ -22,4 +22,4 @@ def vector_embedding(chunked_docs):
 
   vectordb= Chroma.from_documents(documents=chunked_docs, embedding=model, persist_directory=r'C:\Users\hyped\Desktop\RAG_Chatbot\db')
 
-  #print(f"Total documents stored in vector DB: {len(vectordb)}") : enable for testing 
+  return vectordb
