82c5ee7b82c5737d0a56aaee97253684f51cc486
Adnan-Akil <hypedbeats04@gmail.com>
2025-07-17 01:14:43 +0400

    -

--- src/file_parser.py
+++ src/file_parser.py
@@ -7,14 +7,6 @@ from langchain_chroma import Chroma
 def chunker(combined_document):
   splitter= RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=200)
   chunked_docs= splitter.split_documents(combined_document)
-  
-  """ 
-  enable for testing: 
-  print(f"Total chunks: {len(chunked_docs)}")
-  for i, chunk in enumerate(chunked_docs[:]):
-    print(f"\n--- Chunk {i + 1} ---")
-    print(chunk.page_content[:]) 
-  """  
   return chunked_docs
 
 def vector_embedding(chunked_docs):
