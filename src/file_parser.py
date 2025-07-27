b21d4e9836ce683208a66256e9becc4d032001c3
Adnan-Akil <hypedbeats04@gmail.com>
2025-07-27 22:18:44 +0400

    initialised VectorStore once only

--- src/file_parser.py
+++ src/file_parser.py
@@ -1,18 +1,23 @@
-from data_loader import combined_doc
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain_huggingface import HuggingFaceEmbeddings
-from sentence_transformers import SentenceTransformer
-from langchain_chroma import Chroma
+from langchain_chroma.vectorstores import Chroma
 
-def chunker(combined_document):
-  splitter= RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=200)
-  chunked_docs= splitter.split_documents(combined_document)
-  return chunked_docs
+embedding_model = HuggingFaceEmbeddings(
+    model_name="sentence-transformers/all-MiniLM-L6-v2",
+    model_kwargs={"device": "cuda"}
+)
 
-def vector_embedding(chunked_docs):
-  model= HuggingFaceEmbeddings(model_name="sentence-transformers/all-MiniLM-L6-v2", model_kwargs={"device": "cpu"})
+splitter = RecursiveCharacterTextSplitter(
+    chunk_size=1200, chunk_overlap=400
+)
 
-  vectordb= Chroma.from_documents(documents=chunked_docs, embedding=model)
-  #persist_directory=r'C:\Users\hyped\Desktop\RAG_Chatbot\db'
+vector_store=Chroma(
+        embedding=embedding_model,
+        persist_directory=r"C:\Users\hyped\Desktop\RAG_Chatbot\db"
+    )
 
-  return vectordb
+def build_index(combined_doc):
+    vector_store._collection.delete()
+    chunks = splitter.split_documents(combined_doc)
+    vector_store.add_documents(chunks)
+    print(f"Indexed {len(chunks)} chunks into Storage")
